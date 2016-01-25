2a0bdd52a9ec144b6919f79b0fe142eac9a16ebc
Prang, Andreas <andreas.prang@bild.de>
2016-01-25 16:37:45 +0100

    Debugged test.go

--- test.go
+++ test.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-		"fmt"
-	   "time"
+  "fmt"
+  "time"
 )
 
 func forEachLoopMaxInteger(max int) int {
   var sum int = 0
   for n := max; n > 0;  n-- {
-//    var foo := n / 17
+    var foo = n / 17
     sum = sum + n
   }
   return max
 }
  
 func main() {
-//  x := 2.7182
   t0 := time.Now().UnixNano() / int64(time.Millisecond)
   forEachLoopMaxInteger(100000000)
   t1 := time.Now().UnixNano() / int64(time.Millisecond)
-  var duration = t1.Sub(t0)
-  
-  fmt.Printf("Go: %.2f\n", duration)
+  var duration = t1 - t0  
 
+  fmt.Printf("Go: %v\n", duration)
 }
